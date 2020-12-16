9f14ddfa87616b77b57db87212e94a890d664ce3
Florencia norry <maflorencia.norry@gmail.com>
2020-12-15 21:54:53 -0300

    no se termina el ejercicio3

--- js/ejercicio2.js
+++ js/ejercicio2.js
@@ -1,28 +1,28 @@
 ciclo = true;
-while (ciclo){
+while (ciclo) {
     let nota = prompt("Ingrese una nota");
-if (nota >= 0 && nota <= 10) {
-    if (nota < 3) {
-        document.write("Muy deficiente");
-        ciclo(false);
-    } else if (nota < 5) {
-        document.write("insuficiente");
-        ciclo(false);
-    } else if (nota < 7) {
-        document.write("suficiente");
-        ciclo(false);
-    } else if (nota < 8) {
-        document.write("Bien");
-        ciclo(false);
-    } else if (nota < 10) {
-        document.write("Notable");
-        ciclo(false);
+    if (nota >= 0 && nota <= 10) {
+        if (nota < 3) {
+            document.write("Muy deficiente");
+            ciclo(false);
+        } else if (nota < 5) {
+            document.write("insuficiente");
+            ciclo(false);
+        } else if (nota < 7) {
+            document.write("suficiente");
+            ciclo(false);
+        } else if (nota < 8) {
+            document.write("Bien");
+            ciclo(false);
+        } else if (nota < 10) {
+            document.write("Notable");
+            ciclo(false);
+        } else {
+            document.write("Sobresaliente");
+            ciclo(false);
+        }
     } else {
-        document.write("Sobresaliente");
-        ciclo(false);
+        alert("numero erroneo");
     }
-} else {
-    alert("La nota ingresada debe ser entre 0 y 10");
-}
 
 }
