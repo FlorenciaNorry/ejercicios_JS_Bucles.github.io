d5a972bf391a3969788365b3e6c7b21e83af46a2
Florencia norry <maflorencia.norry@gmail.com>
2020-12-21 21:22:16 -0300

    se realiza el ejercicio13

--- js/ejercicio12.js
+++ js/ejercicio12.js
@@ -1,8 +1,4 @@
-// let numeros = [];
-// for(i=1; i<99; i++){
-//     numeros[i] = i;   
-// }
-// let aleatorio = Math.random(1 ,100);
-// document.write(aleatorio);
+//Math.floor: redondea al número entero menor. 
+//Math.random: devuelve un número aleatorio mayor que 0 y menor que 1
 var num = Math.floor((Math.random() * 99) + 1); 
 document.write(num);
---
+++ js/ejercicio13.js
@@ -0,0 +1,2 @@
+let texto = prompt("Ingres un texto en minuscula");
+document.write(texto.toUpperCase());
