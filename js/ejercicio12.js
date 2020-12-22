06dfb5150c24cdc86d8a038fc7b1f45697c2f255
Florencia norry <maflorencia.norry@gmail.com>
2020-12-21 21:17:35 -0300

    se realiza el ejercicio12

---
+++ js/ejercicio12.js
@@ -0,0 +1,8 @@
+// let numeros = [];
+// for(i=1; i<99; i++){
+//     numeros[i] = i;   
+// }
+// let aleatorio = Math.random(1 ,100);
+// document.write(aleatorio);
+var num = Math.floor((Math.random() * 99) + 1); 
+document.write(num);
