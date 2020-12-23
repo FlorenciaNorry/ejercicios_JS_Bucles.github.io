20e1da6b894b5eb579450981ad58490880372e47
Florencia norry <maflorencia.norry@gmail.com>
2020-12-23 20:40:59 -0300

    se realiza el ejercicio14

--- js/ejercicio14.js
+++ js/ejercicio14.js
@@ -1,5 +1,15 @@
 let texto = prompt("Ingrese un texto");
-for (i= 0; i<texto.length; i++){
-    let cadena = texto.split();
-    document.write(cadena + " - ")
-}
+let ltexto = texto.split("");
+    document.write('<p>La cadena original es: "' + texto + '"');
+   document.write("<br>El array tiene " + ltexto.length + " elementos: ");
+
+   for (let i=0; i < ltexto.length; i++) {
+      document.write(ltexto[i] + " - ");
+   }
+
+
+
+// for (i= 0; i<texto.length; i++){
+//     let cadena = texto.split();
+//     document.write(cadena + " - ")
+// }
